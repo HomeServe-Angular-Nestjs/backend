7b2fb6059839b1a73de68f2ab5b2c9779057706c
Muhammed Sajid S <sajid189210@gmail.com>
2025-07-26 13:11:07 +0530

    fix(signup):  errors due to mappers

--- src/modules/auth/services/implementations/signup.service.ts
+++ src/modules/auth/services/implementations/signup.service.ts
@@ -1,10 +1,8 @@
 import {
-    CUSTOMER_REPOSITORY_INTERFACE_NAME, PROVIDER_REPOSITORY_INTERFACE_NAME
+  CUSTOMER_REPOSITORY_INTERFACE_NAME, PROVIDER_REPOSITORY_INTERFACE_NAME
 } from '@core/constants/repository.constant';
 import { OTP_SERVICE_INTERFACE_NAME } from '@core/constants/service.constant';
 import { ARGON_UTILITY_NAME } from '@core/constants/utility.constant';
-import { Customer } from '@core/entities/implementation/customer.entity';
-import { Provider } from '@core/entities/implementation/provider.entity';
 import { ICustomerRepository } from '@core/repositories/interfaces/customer-repo.interface';
 import { IProviderRepository } from '@core/repositories/interfaces/provider-repo.interface';
 import { IArgonUtility } from '@core/utilities/interface/argon.utility.interface';
@@ -24,7 +22,7 @@ export class SignupService implements ISignupService {
     private readonly otpService: IOtpService,
     @Inject(ARGON_UTILITY_NAME)
     private readonly argon: IArgonUtility,
-  ) {}
+  ) { }
 
   // Initiates signup
   async initiateSignup(dto: InitiateSignupDto): Promise<void> {
@@ -39,23 +37,19 @@ export class SignupService implements ISignupService {
     const hashedPassword = await this.argon.hash(dto.password);
 
     if (dto.type === 'customer') {
-      await this.customerRepository.create(
-        new Customer({
-          email: dto.email,
-          username: dto.username,
-          password: hashedPassword,
-          isActive: true,
-        }),
-      );
+      await this.customerRepository.create({
+        email: dto.email,
+        username: dto.username,
+        password: hashedPassword,
+        isActive: true,
+      });
     } else if (dto.type === 'provider') {
-      await this.providerRepository.create(
-        new Provider({
-          email: dto.email,
-          username: dto.username,
-          password: hashedPassword,
-          isActive: true,
-        }),
-      );
+      await this.providerRepository.create({
+        email: dto.email,
+        username: dto.username,
+        password: hashedPassword,
+        isActive: true,
+      });
     }
   }
 
