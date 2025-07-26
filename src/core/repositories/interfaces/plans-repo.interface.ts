c4baca0b42f9ecccaf7f9c512a6e687fcf16c061
Muhammed Sajid S <sajid189210@gmail.com>
2025-07-26 13:08:13 +0530

    fix: plan repository interface

--- src/core/repositories/interfaces/plans-repo.interface.ts
+++ src/core/repositories/interfaces/plans-repo.interface.ts
@@ -1,4 +1,4 @@
 import { IBaseRepository } from '@core/repositories/base/interfaces/base-repo.interface';
-import { PlanDocumentType } from '@core/schema/plans.schema';
+import { PlanDocument } from '@core/schema/plans.schema';
 
-export interface IPlanRepository extends IBaseRepository<PlanDocumentType> { }
+export interface IPlanRepository extends IBaseRepository<PlanDocument> { }
