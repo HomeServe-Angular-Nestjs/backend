f2a82c9b586282c6f48b927c4f7072612592a048
Muhammed Sajid S <sajid189210@gmail.com>
2025-07-28 12:46:12 +0530

    refactor(wsExceptionFilter) added a logger

--- src/core/exception-filters/ws-exception.filters.ts
+++ src/core/exception-filters/ws-exception.filters.ts
@@ -1,11 +1,23 @@
-import { ArgumentsHost, BadRequestException, Catch, WsExceptionFilter } from '@nestjs/common';
+import { ICustomLogger } from '@core/logger/interface/custom-logger.interface';
+import { ILoggerFactory, LOGGER_FACTORY } from '@core/logger/interface/logger-factory.interface';
+import { ArgumentsHost, BadRequestException, Catch, Inject, WsExceptionFilter } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
 
 @Catch(BadRequestException, WsException)
 export class GlobalWsExceptionFilter implements WsExceptionFilter {
+    private readonly logger: ICustomLogger;
+
+    constructor(
+        @Inject(LOGGER_FACTORY)
+        private readonly _loggerFactory: ILoggerFactory
+    ) {
+        this.logger = this._loggerFactory.createLogger(GlobalWsExceptionFilter.name);
+    }
+
     catch(exception: any, host: ArgumentsHost) {
         const client = host.switchToWs().getClient();
 
+        this.logger.error(exception.message);
         if (exception instanceof BadRequestException) {
             client.emit('error', {
                 type: 'validation',
