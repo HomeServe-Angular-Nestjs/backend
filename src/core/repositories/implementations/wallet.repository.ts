1afddaada657675e2c79501e127c92c303413bdd
Muhammed Sajid S <sajid189210@gmail.com>
2025-08-16 19:17:56 +0530

    refactor(wallet): updated the repository with DI

--- src/core/repositories/implementations/wallet.repository.ts
+++ src/core/repositories/implementations/wallet.repository.ts
@@ -1,11 +1,16 @@
+import { WALLET_MODEL_NAME } from "@core/constants/model.constant";
 import { BaseRepository } from "@core/repositories/base/implementations/base.repository";
-import { IWalletRepositoryInterface } from "@core/repositories/interfaces/wallet-repo.interface";
+import { IWalletRepository } from "@core/repositories/interfaces/wallet-repo.interface";
 import { WalletDocument } from "@core/schema/wallet.schema";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import { Model } from "mongoose";
 
 @Injectable()
-export class WalletRepository extends BaseRepository<WalletDocument> implements IWalletRepositoryInterface {
-    // constructor(
-    //     @Inject()
-    // ) { }
+export class WalletRepository extends BaseRepository<WalletDocument> implements IWalletRepository {
+    constructor(
+        @Inject(WALLET_MODEL_NAME)
+        private readonly _walletModel: Model<WalletDocument>
+    ) {
+        super(_walletModel);
+    }
 }
