b182c5ebd8d048295315c49ed70cb333fdaa9aa5
Muhammed Sajid S <sajid189210@gmail.com>
2025-08-22 16:56:46 +0530

    feat(notification) added mappers

---
+++ src/core/dto-mapper/implementation/notification.mapper.ts
@@ -0,0 +1,32 @@
+import { INotificationMapper } from "@core/dto-mapper/interface/notification.mapper.interface";
+import { Notification } from "@core/entities/implementation/notification.entity";
+import { INotification } from "@core/entities/interfaces/notification.entity.interface";
+import { NotificationDocument } from "@core/schema/notification.schema";
+import { Injectable } from "@nestjs/common";
+import { Types } from "mongoose";
+
+@Injectable()
+export class NotificationMapper implements INotificationMapper {
+    toEntity(doc: NotificationDocument): INotification {
+        return new Notification({
+            id: (doc._id as Types.ObjectId).toString(),
+            userId: doc.userId,
+            type: doc.type,
+            title: doc.title,
+            message: doc.message,
+            isRead: doc.isRead,
+            updatedAt: new Date(doc.updatedAt),
+            createdAt: new Date(doc.createdAt)
+        });
+    }
+
+    toDocument(entity: Omit<INotification, 'id'>): Partial<NotificationDocument> {
+        return {
+            userId: entity.userId,
+            type: entity.type,
+            title: entity.title,
+            message: entity.message,
+            isRead: entity.isRead,
+        };
+    }
+}
