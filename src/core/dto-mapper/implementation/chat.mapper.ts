f82f7d59ba08e156e5486cc0d30109120cba7ba4
Muhammed Sajid S <sajid189210@gmail.com>
2026-01-29 16:53:09 +0530

    fix: ci/cd and update the chat and message mapper with proper id convertion.

--- src/core/dto-mapper/implementation/chat.mapper.ts
+++ src/core/dto-mapper/implementation/chat.mapper.ts
@@ -3,12 +3,13 @@ import { Chat } from "@core/entities/implementation/chat.entity";
 import { IChat } from "@core/entities/interfaces/chat.entity.interface";
 import { ChatDocument } from "@core/schema/chat.schema";
 import { Injectable } from "@nestjs/common";
+import { Types } from "mongoose";
 
 @Injectable()
 export class ChatMapper implements IChatMapper {
     toEntity(doc: ChatDocument): IChat {
         return new Chat({
-            id: doc.id,
+            id: (doc._id as Types.ObjectId).toString(),
             participants: doc.participants,
             blockedInfo: doc.blockedInfo,
             createdAt: doc.createdAt,
